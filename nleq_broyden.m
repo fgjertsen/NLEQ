e954302662a8e4de2ca0a69bd2aeef6249ea0508
Frebbeb <fredrik.gjertsen@gmail.com>
2022-02-26 20:34:00 +0100

    Cosmetics

--- nleq_broyden.m
+++ nleq_broyden.m
@@ -19,71 +19,71 @@
 %
 function xm = nleq_broyden(xv0, f, par, J, tol, nItMax)
 
-	% Initiate function values
-	xv = xv0;
-	n  = length(xv);
-	fr = feval(f, xv, par);
-	
-	% Initiate Broyden approximation to Jacobian
-	if not( isempty ( J ) )
+    % Initiate function values
+    xv = xv0;
+    n  = length(xv);
+    fr = feval(f, xv, par);
+    
+    % Initiate Broyden approximation to Jacobian
+    if not( isempty ( J ) )
         Br = inv( feval( J, xv, par) );
-	else
+    else
         Br = eye(n);
-	end
-	M = zeros(n);
-	
-	% Make sure tolerance is set
-	if isempty(tol)
+    end
+    M = zeros(n);
+    
+    % Make sure tolerance is set
+    if isempty(tol)
         tol = 1.e-10;
-	end
-	
-	% Make sure max. iterations is set
-	if isempty(nItMax)
+    end
+    
+    % Make sure max. iterations is set
+    if isempty(nItMax)
         nItMax = 1000;
-	end
-	
-	% Allocate solutions matrix, with sufficient space
-	xm = zeros(n, nItMax+1);
-	
-	% Tuning parameter of Broydens method
-	tau = 1;
-	
-	% Start iteration loop
-	bConverged = 0;
-	nIt = 0;
-	while ~bConverged
-		
-		% Save iterates
-		xm(:, nIt+1) = xv;
-		
-		% Check for convergence
-		if norm(fr) < tol || nIt >= nItMax
+    end
+    
+    % Allocate solutions matrix, with sufficient space
+    xm = zeros(n, nItMax+1);
+    
+    % Tuning parameter of Broydens method
+    tau = 1;
+    
+    % Start iteration loop
+    bConverged = 0;
+    nIt = 0;
+    while ~bConverged
+        
+        % Save iterates
+        xm(:, nIt+1) = xv;
+        
+        % Check for convergence
+        if norm(fr) < tol || nIt >= nItMax
             bConverged = 1;
             break;
-		end
-		
-		% Do "Newton Iteration" step
-		pr = -Br * fr;
-		xv = xv + tau*pr;
-		
-		% Update Jacobian approximation, using Broydens formula
-		dfr = feval(f, xv, par) - fr;
-		fr  = fr + dfr;
-		oyp = Br*dfr - pr;
-		pB  = pr' * Br;
-		for i=1:n
-			for j=1:n
-				M(i,j) = oyp(i) * pB(j);
-			end
-		end
-		Br = Br - M./(pr'*Br*dfr);
-		
-		% Increment loop counter
-		nIt = nIt + 1;
-		
-	end
-	
-	% Return the solutions from the iterations
-	xm = xm(:, 1:nIt+1);
+        end
+        
+        % Do "Newton Iteration" step
+        pr = -Br * fr;
+        xv = xv + tau*pr;
+        
+        % Update Jacobian approximation, using Broydens formula
+        dfr = feval(f, xv, par) - fr;
+        fr  = fr + dfr;
+        oyp = Br*dfr - pr;
+        pB  = pr' * Br;
+        for i=1:n
+            for j=1:n
+                M(i,j) = oyp(i) * pB(j);
+            end
+        end
+        Br = Br - M./(pr'*Br*dfr);
+        
+        % Increment loop counter
+        nIt = nIt + 1;
+        
+    end
+    
+    % Return the solutions from the iterations
+    xm = xm(:, 1:nIt+1);
 
 end
